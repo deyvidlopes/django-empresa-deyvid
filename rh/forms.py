669f794a64aecb3997b1a20e42f3c3ca2966e949
Deyvid da Costa Lopes <103329751+deyvidlopes@users.noreply.github.com>
2025-11-17 20:01:48 -0300

    Update forms.py

--- rh/forms.py
+++ rh/forms.py
@@ -1,28 +1,47 @@
-# contato/forms.py
 from django import forms
 from .models import MensagemContato
+from django.contrib.auth.models import User #from para login
 
 class ContatoModelForm(forms.ModelForm):
     
     class Meta:
-        # 1. Especifica o modelo que este formulário irá usar
         model = MensagemContato
         
-        # 2. Especifica os campos do modelo que queremos exibir no formulário.
-        #    Note que 'data_envio' e 'lido' não estão aqui, pois
-        #    eles são definidos automaticamente (default) e não pelo usuário.
-        fields = ['nome', 'email', 'assunto', 'mensagem']
+        # CAMPOS ATUALIZADOS
+        fields = ['nome', 'email', 'celular', 'assunto', 'descricao']
 
-        # 3. (Opcional) Personaliza os widgets para o HTML
+        # WIDGETS ATUALIZADOS
         widgets = {
             'nome': forms.TextInput(attrs={'placeholder': 'Seu nome completo', 'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'placeholder': 'seu-email@exemplo.com', 'class': 'form-control'}),
+            'celular': forms.TextInput(attrs={'placeholder': 'Ex: (XX) XXXXX-XXXX', 'class': 'form-control'}),
             'assunto': forms.TextInput(attrs={'placeholder': 'Assunto da mensagem', 'class': 'form-control'}),
-            'mensagem': forms.Textarea(attrs={'rows': 5, 'placeholder': 'Digite sua mensagem...', 'class': 'form-control'}),
+            'descricao': forms.Textarea(attrs={'rows': 5, 'placeholder': 'Digite sua mensagem...', 'class': 'form-control'}),
         }
         
-        # 4. (Opcional) Personaliza os labels (rótulos)
+        # LABELS ATUALIZADOS
         labels = {
             'nome': 'Nome Completo',
             'email': 'Seu E-mail',
+            'celular': 'Celular/Contato',
+            'descricao': 'Descrição (Mensagem)',
         }
+
+#form de login
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Usuário')
+    password = forms.CharField(label='Senha',widget=forms.PasswordInput)
+
+class RegistroForm(forms.ModelForm):
+    password = forms.CharField(label='Senha', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirme a senha',widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username','email')
+
+    def clean(self):
+        cleaned = super().clean()
+        if cleaned.get('password') != cleaned.get('password2'):
+            raise forms.ValidationError('Senhas diferentes')
+        return cleaned        
